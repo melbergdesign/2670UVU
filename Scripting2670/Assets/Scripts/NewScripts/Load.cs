a850937470b2a0f0c21970aa3822beae4d1af746
Anthony Romrell <anthonyromrell@mac.com>
2017-09-15 13:24:33 -0600

    Updated Character Assets

---
+++ Scripting2670/Assets/Scripts/NewScripts/Load.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Load : MonoBehaviour {
+
+	public static Action EndButtons;
+
+	void Start()
+	{
+		Load.EndButtons += EndThis;
+	}
+
+    private void EndThis()
+    {
+        GetComponent<Button>().interactable = false;
+    }
+
+    public string scene;
+
+	public void LoadLevel () {
+		SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+		EndButtons();
+	}
+
+}
