88ef98149d1d94f6e3e09e84504f35442a16ccc0
Anthony Romrell <anthonyromrell@mac.com>
2017-09-18 15:17:28 -0600

    Fire Weapons

--- Scripting2670/Assets/Scripts/NewScripts/MoveInput.cs
+++ Scripting2670/Assets/Scripts/NewScripts/MoveInput.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 using System;
 public class MoveInput : MonoBehaviour {
 
-<<<<<<< HEAD:Scripting2670/Assets/Scripts/MoveInput.cs
 	public float runTime = 0.01f;
-	public static Action<float> KeyAction;
-=======
 	public static Action<float> MoveAction;
->>>>>>> LearningAssets:Scripting2670/Assets/Scripts/NewScripts/MoveInput.cs
 	public static Action JumpAction;
 	public bool canPlay = true;
 
@@ -25,19 +21,12 @@ public class MoveInput : MonoBehaviour {
 				JumpAction();
 			}
 
-
-<<<<<<< HEAD:Scripting2670/Assets/Scripts/MoveInput.cs
-			if (KeyAction != null)
+			if (MoveAction != null)
 			{
-				KeyAction(Input.GetAxis("Horizontal"));
+				MoveAction(Input.GetAxis("Horizontal"));
 			} 
 			yield return new WaitForSeconds(runTime);
 		}
-=======
-		if (MoveAction != null)
-		{
-			MoveAction(Input.GetAxis("Horizontal"));
-		} 
->>>>>>> LearningAssets:Scripting2670/Assets/Scripts/NewScripts/MoveInput.cs
+
 	}
 }
---
+++ Scripting2670/Assets/Scripts/WeaponClass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponClass : MonoBehaviour {
+
+	public int ammoCount = 100;
+	public float fireRate = 1;
+	public float ammoPower = 0.1f;
+	public bool canFire = true;
+
+	void Awake()
+	{
+		FireInput.FireAction += FireHandler;
+		FireInput.StopAction += StopHandler;
+	}
+
+	void FireHandler () {
+		StartCoroutine(Fire());
+	}
+
+	void StopHandler () {
+		StopAllCoroutines();
+		print("Stop");
+	}
+	
+	public IEnumerator Fire () {
+		print("Start");
+		while (canFire)
+		{
+			if (ammoCount > 0)
+			{
+				print(ammoCount + this.name);
+				ammoCount--;
+				yield return new WaitForSeconds(fireRate);
+			} else {
+				canFire = false;
+			}
+		}
+		print("Out of Ammo.");
+	}	
+}
