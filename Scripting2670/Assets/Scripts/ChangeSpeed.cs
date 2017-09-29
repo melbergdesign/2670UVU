fdad0d3a55c01322dc9d0b223fe3a93fbc1b1883
Anthony Romrell <anthonyromrell@mac.com>
2017-09-29 15:25:51 -0600

    Water

--- Scripting2670/Assets/Scripts/ChangeSpeed.cs
+++ Scripting2670/Assets/Scripts/ChangeSpeed.cs
@@ -7,8 +7,26 @@ public class ChangeSpeed : MonoBehaviour {
 
 	public static Action<float, float> SendSpeed;
 	
+	public StaticVars.GameSpeed speedType;
+
+
 	void OnTriggerEnter () {
-		SendSpeed(StaticVars.waterSpeed, StaticVars.waterGravity);
+		
+		switch (speedType)
+		{
+			case StaticVars.GameSpeed.DRAG:
+				SendSpeed(StaticVars.dragSpeed, StaticVars.dragGravity);
+				break;
+
+			case StaticVars.GameSpeed.BOOST:
+				SendSpeed(StaticVars.boostSpeed, StaticVars.boostGravity);
+				break;
+
+			case StaticVars.GameSpeed.SUPERBOOST:
+				SendSpeed(StaticVars.boostSpeed*10, StaticVars.boostGravity);
+				break;
+			
+		}
 	}
 
 	void OnTriggerExit()
--- Scripting2670/Assets/Scripts/GameData.cs
+++ Scripting2670/Assets/Scripts/GameData.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class GameData : MonoBehaviour {
 
 	public float speed = 40;
+	public float dragSpeed = 10;
+	public float boostSpeed = 80;
+
+
 	public float gravity = 15;
-	public float waterSpeed = 10;
-	public float waterGravity = 5;
+	public float dragGravity = 5;
+	public float boostGravity = 30;
+	
+	
 
 	// Use this for initialization
 	void Start () {
 		StaticVars.speed = speed;
+		StaticVars.dragSpeed = dragSpeed;
+		StaticVars.boostSpeed = boostSpeed;
+
 		StaticVars.gravity = gravity;
-		StaticVars.waterSpeed = waterSpeed;
-		StaticVars.waterGravity = waterGravity;
+		StaticVars.dragGravity = dragGravity;
+		StaticVars.boostGravity = boostGravity;
 	}
 
 }
