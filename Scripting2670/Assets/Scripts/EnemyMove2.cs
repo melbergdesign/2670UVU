61b9f97d02eef7a18c4dd7685314f5c211cd0148
Anthony Romrell <anthonyromrell@mac.com>
2017-09-29 14:05:16 -0600

    Adding NavMesh AI

---
+++ Scripting2670/Assets/Scripts/EnemyMove2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMove2 : MonoBehaviour {
+	private NavMeshAgent agent;
+	private Transform player;
+	void Awake () {
+		agent = GetComponent<NavMeshAgent>();
+		SendToEnemy.SendTransform += SendTransformHandler;
+	}
+
+    private void SendTransformHandler(Transform _transform)
+    {
+        player = _transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		agent.destination = player.position;
+	}
+}
