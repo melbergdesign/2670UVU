50807870c558064a53ac8202a99863ba754e3393
Unknown <melbergdesign@gmail.com>
2017-10-03 12:28:18 -0600

    Bear Protoype

---
+++ Bear_Prototypes/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public CharacterController cc;
+	public Vector3 moveChar;
+
+	public float horMove;
+	public float vertMove;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+
+		moveChar.x = horMove;
+		moveChar.y = vertMove;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cc.Move (moveChar);
+	}
+}
