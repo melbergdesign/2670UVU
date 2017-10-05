b06fd7311488d2ae3f6ccc64b16a10ea1c33af7d
Unknown <melbergdesign@gmail.com>
2017-10-05 12:39:04 -0600

    Vine Trigger Works

---
+++ Bear_Prototypes/Assets/Scripts/climbableVines.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class climbableVines : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other){
+		print ("Touching Vines");
+	}
+
+	void OnTriggerExit (Collider other){
+		print ("Not Touching Vines");
+	}
+}
