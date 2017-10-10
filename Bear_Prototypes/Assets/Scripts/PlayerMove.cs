15b4f690a1ca487178ca9ad4fa39a4b2a8f5ff55
Unknown <melbergdesign@gmail.com>
2017-10-10 13:06:35 -0600

    Worked on push physics, added tree (0.5 hrs)

--- Bear_Prototypes/Assets/Scripts/PlayerMove.cs
+++ Bear_Prototypes/Assets/Scripts/PlayerMove.cs
@@ -56,4 +56,5 @@ public class PlayerMove : MonoBehaviour {
 
 		cc.Move (moveChar);
 	}
+		
 }
---
+++ Bear_Prototypes/Assets/Scripts/PushObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushObject : MonoBehaviour {
+
+	public float pushPower = 2.0f;
+
+	void OnControllerColliderHit (ControllerColliderHit hit){
+		print ("hit");
+	}
+}
