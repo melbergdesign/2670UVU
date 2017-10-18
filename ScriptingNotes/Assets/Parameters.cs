5338444967fc9db1648c66187be86e92556758e3
Unknown <melbergdesign@gmail.com>
2017-10-18 15:41:25 -0600

    Style guide work, notes on get & set data

---
+++ ScriptingNotes/Assets/Parameters.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parameters : MonoBehaviour {
+	[SerializeField] // can be seen in the Unity editor
+	private float speed = 0; //field; directly getting and setting data
+	//cannot be accessed by any other data
+
+	public float Speed{
+		get{return speed;}
+		set{
+			if (speed < 100){//check data
+				speed += value;
+				}// value is a keyword
+		}
+	}//parameter; indirectly getting and setting data
+	//public to scripts, but not the unity editor
+	//can choose if something can get or set data
+
+	// Use this for initialization
+	void OnMouseDown () {
+		Speed = 20;
+		print (Speed);
+		
+	}
+	
+	// Update is called once per frame
+
+	//able to create data that cannot be changed by outside influences
+	//decide which data is accessible
+}
