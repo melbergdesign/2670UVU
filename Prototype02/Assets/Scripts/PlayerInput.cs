5c8a31afce8b288e706abc4271b75eb824e2a213
Unknown <melbergdesign@gmail.com>
2017-11-15 12:44:21 -0700

    Reworked input script (.5hr)

---
+++ Prototype02/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public PlayerMove myMove;
+
+	// Use this for initialization
+	void Start () {
+		myMove = GetComponent<PlayerMove> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0) {
+			//print ("Right");
+
+			myMove.TestFunc ();
+		}
+		
+	}
+}
